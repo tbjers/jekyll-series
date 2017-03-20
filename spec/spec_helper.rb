1ec51680586f3b53bc2442e883e186c041185a05
Torgny Bjers <torgny.bjers@gmail.com>
2017-03-19 20:14:03 -0400

    Ensuring that specs run correctly

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,15 @@
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+
 require "bundler/setup"
+require "support/jekyll_template"
 require "jekyll/series"
+require "byebug" if RUBY_VERSION >= "2.0"
 
 RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
